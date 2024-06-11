f33dabcb909d94440b158c9c4302311221708957
tersalman <mansalmanter@gmail.com>
2024-06-11 12:55:09 +0300

    adding Entity and repositories with mapper

---
+++ src/main/java/ru/skypro/homework/mapper/CommentMapper.java
@@ -0,0 +1,14 @@
+package ru.skypro.homework.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import ru.skypro.homework.dto.CommentDTO;
+import ru.skypro.homework.dto.CreateOrUpdateComment;
+import ru.skypro.homework.modal.CommentEntity;
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface CommentMapper {
+    CommentEntity toComment(CommentDTO commentDTO);
+    CommentEntity toComment(CreateOrUpdateComment createdComment);
+    CommentDTO toDto(CommentEntity commentEntity);
+    CreateOrUpdateComment toCreatedComment(CommentEntity commentEntity);
+}
